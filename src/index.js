2d96087d690d5ba89e3acd87d2266ed8d20990cc
Xiao Luo <xiao2017luo@gmail.com>
2017-10-07 22:06:25 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Content from './Content';
+
+
+ReactDOM.render(
+  <Content />,
+  document.getElementById('content')
+);
